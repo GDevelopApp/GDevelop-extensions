acb1acef312dbc0dc74df873bde826c2452dd6a2
Florian Rival <Florian.rival@gmail.com>
2021-07-10 12:19:11 +0100

    Add continuous integration instead of manual build and avoid serving extensions from GitHub (#178)

---
+++ __tests__/post-build/extensions.spec.js
@@ -0,0 +1,55 @@
+// @ts-check
+const fs = require('fs').promises;
+const path = require('path');
+
+/** @typedef {import('../../scripts/types').ExtensionsDatabase} ExtensionsDatabase */
+
+const distExtensionsPath = path.join(__dirname, '../../dist/extensions');
+
+/** @return {Promise<ExtensionsDatabase]>} */
+const getExtensionsDatabase = async () => {
+  const extensionsDatabase = await fs.readFile(
+    path.join(
+      __dirname,
+      '../../dist/extensions-database/extensions-database.json'
+    )
+  );
+  const parsedExtensionsDatabase = JSON.parse(extensionsDatabase.toString());
+  return parsedExtensionsDatabase;
+};
+
+describe('extensions database post check', () => {
+  test('extensions-database.json', async () => {
+    const extensionsDatabase = await getExtensionsDatabase();
+
+    // Check that the headers seem correct
+    expect(extensionsDatabase.extensionShortHeaders.length).toBeGreaterThan(70);
+
+    expect(
+      extensionsDatabase.extensionShortHeaders.find(
+        ({ name }) => name === 'DraggableSliderControl'
+      )
+    ).toBeDefined();
+    expect(
+      extensionsDatabase.extensionShortHeaders.find(
+        ({ name }) => name === 'FireBullet'
+      )
+    ).toBeDefined();
+    expect(
+      extensionsDatabase.extensionShortHeaders.find(
+        ({ name }) => name === 'FollowObjectsWithCamera'
+      )
+    ).toBeDefined();
+  });
+  test('extensions', async () => {
+    // Check that extensions are present.
+    await expect(
+      fs.stat(path.join(distExtensionsPath, '/FireBullet.json'))
+    ).resolves.toBeDefined();
+    await expect(
+      fs.stat(
+        path.join(distExtensionsPath, '/FireBullet-header.json')
+      )
+    ).resolves.toBeDefined();
+  });
+});
---
+++ scripts/generate-extensions-registry.js
@@ -0,0 +1,149 @@
+const path = require('path');
+const fs = require('fs').promises;
+const shell = require('shelljs');
+
+const extensionsBasePath = path.join(__dirname, '..', 'Extensions');
+const distBasePath = path.join(__dirname, '..', 'dist');
+const distDatabasesPath = path.join(distBasePath, 'extensions-database');
+const distExtensionsPath = path.join(distBasePath, 'extensions');
+const extensionsBaseUrl = 'https://resources.gdevelop-app.com/extensions';
+
+/** @typedef {import('./types').ExtensionShortHeader} ExtensionShortHeader */
+/** @typedef {import('./types').ExtensionsDatabase} ExtensionsDatabase */
+/** @typedef {import('./types').ExtensionHeader} ExtensionHeader */
+
+/**
+ * @param {string} path
+ * @param {any} object
+ * @returns {Promise<void>}
+ */
+const writeJSONFile = (path, object) =>
+  fs.writeFile(path, JSON.stringify(object, null, 2));
+
+const readAllExtensions = async () => {
+  const filenames = await fs.readdir(extensionsBasePath);
+  const filteredFilenames = filenames.filter((name) => name.endsWith('.json'));
+
+  return await Promise.all(
+    filteredFilenames.map(async (filename) => {
+      const content = await fs.readFile(
+        path.join(extensionsBasePath, filename),
+        'utf8'
+      );
+
+      return {
+        filename,
+        extension: JSON.parse(content),
+      };
+    })
+  );
+};
+
+(async () => {
+  try {
+    shell.mkdir('-p', distBasePath);
+    shell.mkdir('-p', distExtensionsPath);
+    shell.mkdir('-p', distDatabasesPath);
+
+    const extensionWithFilenames = await readAllExtensions();
+
+    const allTagsSet = new Set();
+
+    /** @type {ExtensionShortHeader[]} */
+    const extensionShortHeaders = [];
+
+    await Promise.all(
+      extensionWithFilenames.map(async (extensionWithFilename) => {
+        const { extension, filename } = extensionWithFilename;
+
+        // Convert back to the old format for tags.
+        if (Array.isArray(extension.tags)) {
+          extension.tags = extension.tags.join(',');
+        }
+
+        const { name } = extension;
+
+        // Do some consistency checks.
+        if (name.endsWith('Extension')) {
+          throw new Error(
+            `Extension names should not finish with \"Extension\". Please rename ${name}.`
+          );
+        }
+
+        if (name + '.json' !== filename) {
+          throw new Error(
+            `Extension filename should be exactly the name of the extension (with .json extension). Please rename ${name} and/or ${filename}.`
+          );
+        }
+
+        // Generate the headers of the extension
+        /** @type {ExtensionShortHeader} */
+        const extensionShortHeader = {
+          shortDescription: extension.shortDescription,
+          extensionNamespace: extension.extensionNamespace,
+          fullName: extension.fullName,
+          name,
+          version: extension.version,
+          url: `${extensionsBaseUrl}/${name}.json`,
+          headerUrl: `${extensionsBaseUrl}/${name}-header.json`,
+          tags: extension.tags,
+          previewIconUrl: extension.previewIconUrl,
+          eventsBasedBehaviorsCount: extension.eventsBasedBehaviors.length,
+          eventsFunctionsCount: extension.eventsFunctions.length,
+        };
+        extensionShortHeaders.push(extensionShortHeader);
+
+        /** @type {ExtensionHeader} */
+        const extensionHeader = {
+          ...extensionShortHeader,
+          description: extension.description,
+          iconUrl: extension.iconUrl,
+        };
+
+        extension.tags.split(',').map(
+          /** @param {string} tag */
+          (tag) => {
+            allTagsSet.add(tag.trim().toLowerCase());
+          }
+        );
+
+        await writeJSONFile(
+          path.join(distExtensionsPath, `${name}.json`),
+          extension
+        );
+
+        await writeJSONFile(
+          path.join(distExtensionsPath, `${name}-header.json`),
+          extensionHeader
+        );
+      })
+    );
+
+    // Write the registry
+    /** @type {ExtensionsDatabase} */
+    const registry = {
+      version: '0.0.1',
+      allTags: Array.from(allTagsSet),
+      extensionShortHeaders,
+    };
+
+    await writeJSONFile(
+      path.join(distDatabasesPath, 'extensions-database.json'),
+      registry
+    );
+
+    // Also write the old format registry so that it's found by
+    // old GDevelop versions.
+    await writeJSONFile(
+      path.join(__dirname, '..', 'extensions-registry.json'),
+      registry
+    );
+
+    console.log(`✅ Headers and registry files successfully updated`);
+  } catch (error) {
+    console.error(
+      `⚠️ Error while generating headers and registry files:`,
+      error
+    );
+  }
+})();
