ecb555e0f76691b2b49f03c6d30fa67197b335a1
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-07-03 23:30:14 +0200

    Make zipPath not optional

--- scripts/extract-extension.js
+++ scripts/extract-extension.js
@@ -6,12 +6,12 @@ const pipeline = require('util').promisify(require('stream').pipeline);
 
 /**
  * Extracts exactly one extension into the community extensions folder from a zip file.
- * @param {string} [zipPath] The path to the zip file to extract.
+ * @param {string} zipPath The path to the zip file to extract.
  * @param {string} [extensionsFolder] The folder with the extensions.
  * @returns {Promise<{error: "too-many-files" | "no-json-found"| "invalid-file-name" | "zip-error", details?: any} | {error?: undefined,extensionName: string}>} the name of the extracted extension if successful, else a generic error code.
  */
 exports.extractExtension = async function (
-  zipPath = '/tmp/ext.zip',
+  zipPath,
   extensionsFolder = `${__dirname}/../extensions`
 ) {
   // Load in the archive with JSZip
