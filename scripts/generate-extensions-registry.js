bdaa9b6c1d0a508e77542ae257457c37654f463c
Davy Hélard <davy.helard@gmail.com>
2022-09-30 11:26:06 +0200

    Ensure category is set in short headers.

--- scripts/generate-extensions-registry.js
+++ scripts/generate-extensions-registry.js
@@ -167,7 +167,7 @@ const readExtensionsFromFolder = async (folderPath, tier) => {
           headerUrl: `${extensionsBaseUrl}/${name}-header.json`,
           //@ts-ignore Conversion to string done above
           tags: extension.tags,
-          category: extension.category,
+          category: extension.category || '',
           previewIconUrl: extension.previewIconUrl,
           eventsBasedBehaviorsCount: extension.eventsBasedBehaviors.length,
           eventsFunctionsCount: extension.eventsFunctions.length,
