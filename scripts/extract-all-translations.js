f82b453a73f2547afcb5618996e39179dca86ec8
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2025-03-17 10:29:20 +0100

    Try extracting translations + upload

---
+++ scripts/extract-all-translations.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const fs = require('fs');
+
+const rootPath = path.join(__dirname, '..');
+const translationsPath = path.join(rootPath, '.translations');
+const reviewedExtensionsPath = path.join(rootPath, 'extensions', 'reviewed');
+
+try {
+  // Clean existing English messages catalog, if any
+  const enMessagesJsPath = path.join(
+    translationsPath,
+    'locales/en/messages.js'
+  );
+  if (fs.existsSync(enMessagesJsPath)) {
+    console.info(
+      `ℹ️ Removing ${enMessagesJsPath} as "en" should not have any translations ("pot" file)`
+    );
+    fs.rmSync(enMessagesJsPath, { recursive: true, force: true });
+  }
+  const enMessagesPotPath = path.join(
+    translationsPath,
+    'reviewed-extensions-messages.pot'
+  );
+  if (fs.existsSync(enMessagesPotPath)) {
+    console.info(
+      `ℹ️ Removing ${enMessagesPotPath} as "en" should not have any translations ("pot" file)`
+    );
+    fs.rmSync(enMessagesPotPath, { recursive: true, force: true });
+  }
+
+  // Step 1: Find JSON files in 'reviewed' folder.
+  console.log("ℹ️ Listing all JSON files in the 'reviewed' folder...");
+  const jsonFiles = fs
+    .readdirSync(reviewedExtensionsPath)
+    .filter((file) => file.endsWith('.json'));
+
+  console.log('ℹ️ Generating .POT file...');
+  let potContent = 'msgid ""\nmsgstr ""\n\n';
+  const translationsMap = new Map();
+
+  jsonFiles.forEach((file) => {
+    const filePath = path.join(reviewedExtensionsPath, file);
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    ['fullName', 'shortDescription', 'description', 'category'].forEach(
+      (key) => {
+        if (data[key]) {
+          const values = Array.isArray(data[key]) ? data[key] : [data[key]];
+          values.forEach((value) => {
+            if (value.trim()) {
+              // Avoid empty strings
+              if (!translationsMap.has(value)) {
+                translationsMap.set(value, []);
+              }
+              translationsMap.get(value).push(`${filePath}`);
+            }
+          });
+        }
+      }
+    );
+  });
+
+  // Step 3: Build POT content
+  translationsMap.forEach((files, value) => {
+    const uniqueFiles = [...new Set(files)];
+    potContent += uniqueFiles.map((file) => `#: ${file}`).join('\n') + '\n';
+    potContent += `msgid "${value.replace(/"/g, '\\"')}"\n`;
+    potContent += 'msgstr ""\n\n';
+  });
+
+  // Step 4: Write to .POT file
+  const potFilePath = path.join(
+    translationsPath,
+    'reviewed-extensions-messages.pot'
+  );
+  fs.writeFileSync(potFilePath, potContent);
+  console.log(
+    "ℹ️ Translation file 'reviewed-messages.pot' generated and ready for translation."
+  );
+} catch (error) {
+  console.error('❌ Error occurred while extracting translations:', error);
+}
---
+++ scripts/lib/Locales.js
@@ -0,0 +1,135 @@
+const fs = require('fs');
+const path = require('path');
+const ISO6391 = require('iso-639-1').default;
+
+const rootPath = path.join(__dirname, '../..');
+const translationsPath = path.join(rootPath, '.translations');
+
+/**
+ * @param {string} langLongCode
+ * @returns {string}
+ */
+const getShortestCode = (langLongCode) => {
+  if (langLongCode === 'pt_BR') return langLongCode;
+
+  const langParts = langLongCode.split('_');
+  return langParts[0];
+};
+
+/**
+ * @returns {Promise<string[]>}
+ */
+const getLocales = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(translationsPath, (error, locales) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(
+        locales
+          .filter((name) => name !== '.DS_Store')
+          .filter((name) => name !== 'LocalesMetadata.js')
+          .filter((name) => name !== '_build')
+      );
+    });
+  });
+};
+
+/**
+ * @param {string} localeName
+ * @returns {string[]}
+ */
+const getLocaleSourceCatalogFiles = (localeName) => {
+  if (localeName === 'en') return ['ide-messages.pot'];
+  if (localeName === 'pseudo_LOCALE') return ['ide-messages.pot'];
+
+  return [
+    'reviewed-extensions-messages.po',
+    'community-extensions-messages.po',
+  ];
+};
+
+/**
+ * @param {string} localeName
+ * @returns {string}
+ */
+const getLocalePath = (localeName) => {
+  return path.join(translationsPath, localeName);
+};
+
+/**
+ * @param {string} localeName
+ * @returns {string}
+ */
+const getLocaleCatalogPath = (localeName) => {
+  return path.join(getLocalePath(localeName), 'messages.po');
+};
+
+/**
+ * @param {string} localeName
+ * @returns {string}
+ */
+const getLocaleCompiledCatalogPath = (localeName) => {
+  return path.join(getLocalePath(localeName), 'messages.js');
+};
+
+const getLocaleMetadataPath = () => {
+  return path.join(translationsPath, 'LocalesMetadata.js');
+};
+
+/**
+ * @param {string} langCode
+ * @returns {string}
+ */
+const getLocaleName = (langCode) => {
+  if (langCode === 'pt_BR') {
+    return 'Brazilian Portuguese';
+  } else if (langCode === 'zh_CN') {
+    return 'Chinese Simplified';
+  } else if (langCode === 'zh_TW') {
+    return 'Chinese Traditional';
+  } else if (langCode === 'sr_CS') {
+    return 'Serbian (Latin)';
+  } else if (langCode === 'fil_PH') {
+    return 'Filipino';
+  } else if (langCode === 'pseudo_LOCALE') {
+    return 'for development only';
+  }
+
+  return ISO6391.getName(getShortestCode(langCode));
+};
+
+/**
+ *
+ * @param {string} langCode
+ * @returns {string}
+ */
+const getLocaleNativeName = (langCode) => {
+  if (langCode === 'pt_BR') {
+    return 'Português brasileiro';
+  } else if (langCode === 'zh_CN') {
+    return '简化字';
+  } else if (langCode === 'zh_TW') {
+    return '正體字';
+  } else if (langCode === 'sr_CS') {
+    return 'srpski';
+  } else if (langCode === 'fil_PH') {
+    return 'Mga Filipino';
+  } else if (langCode === 'pseudo_LOCALE') {
+    return 'Pseudolocalization';
+  }
+
+  return ISO6391.getNativeName(getShortestCode(langCode));
+};
+
+module.exports = {
+  getLocales,
+  getLocalePath,
+  getLocaleSourceCatalogFiles,
+  getLocaleCatalogPath,
+  getLocaleCompiledCatalogPath,
+  getLocaleMetadataPath,
+  getLocaleName,
+  getLocaleNativeName,
+};
